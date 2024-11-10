1cb9a13314477fac31a9a2d43439e8c18773e2bf
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-11-10 16:27:30 +0530

    added songs route

--- server/src/routes/song.route.js
+++ server/src/routes/song.route.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import {
+  getAllSongs,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/song.controller.js";
+import { protectedRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-
-router.get("/", (req, res) => {
-    res.send("Welcome to the Song Route");
-})
-
+router.get("/", protectedRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/madeForYou", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
