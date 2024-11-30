875117ee5865b7b8bd21a16b70df279077ecb8a6
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-11-30 12:45:19 +0530

    main layout complete

--- client/src/store/useMusicStore.ts
+++ client/src/store/useMusicStore.ts
@@ -8,8 +8,10 @@ interface MusicStore {
     songs: Song[];
     isLoading: boolean;
     error: string | null;
+    currentAlbum: null | Album;
 
-    fetchAlbums: () => void;
+    fetchAlbums: () => Promise<void>;
+    fetchAlbumById : (id:string) => Promise<void>;
 }
 
 export const useMusicStore= create<MusicStore>((set)=>({
@@ -17,6 +19,7 @@ export const useMusicStore= create<MusicStore>((set)=>({
 songs:[],
 isLoading:false,
 error:null,
+currentAlbum:null ,
 
 fetchAlbums : async()=>{
     set({isLoading:true,error:null})
@@ -28,5 +31,16 @@ fetchAlbums : async()=>{
     }finally{
         set({isLoading:false})
     }
-}
+ }, 
+ fetchAlbumById : async(id) => {
+     set({isLoading:true,error:null})
+     try {
+        const response = await axiosInstance.get("/albums/"+id)
+        set({currentAlbum:response.data})
+     } catch (error:any) {
+        set({error:error.response.data.message})
+     }finally{
+         set({isLoading:false})
+     }
+ },
 }))
