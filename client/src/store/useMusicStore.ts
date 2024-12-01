7faad0633b1a5550da3f1e8bfbe542f3c743650f
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-12-01 16:07:37 +0530

    home page complete

--- client/src/store/useMusicStore.ts
+++ client/src/store/useMusicStore.ts
@@ -9,9 +9,15 @@ interface MusicStore {
     isLoading: boolean;
     error: string | null;
     currentAlbum: null | Album;
+    featuredSongs: Song[];
+    madeForYouSongs:Song[];
+    trendingSongs:Song[];
 
     fetchAlbums: () => Promise<void>;
     fetchAlbumById : (id:string) => Promise<void>;
+    fetchFeaturedSongs: () => Promise<void>;
+    fetchMadeForYouSongs: () => Promise<void>;
+    fetchTrendingSongs: () => Promise<void>;
 }
 
 export const useMusicStore= create<MusicStore>((set)=>({
@@ -20,7 +26,9 @@ songs:[],
 isLoading:false,
 error:null,
 currentAlbum:null ,
-
+madeForYouSongs:[],
+featuredSongs:[],
+trendingSongs:[],
 fetchAlbums : async()=>{
     set({isLoading:true,error:null})
     try {
@@ -43,4 +51,37 @@ fetchAlbums : async()=>{
          set({isLoading:false})
      }
  },
+ fetchFeaturedSongs : async() => {
+    set({isLoading:true,error:null})
+    try {
+        const response = await axiosInstance.get("/songs/featured")
+        set({featuredSongs:response.data})        
+    } catch (error:any) {
+        set({error:error.response.data.message})
+    }finally{
+        set({isLoading:false})
+    }
+ } ,
+ fetchMadeForYouSongs : async() => {
+    set({isLoading:true,error:null})
+    try {
+        const response = await axiosInstance.get("/songs/madeForYou")
+        set({madeForYouSongs:response.data})        
+    } catch (error:any) {
+        set({error:error.response.data.message})
+    }finally{
+        set({isLoading:false})
+    }
+ } ,
+ fetchTrendingSongs : async() => {
+    set({isLoading:true,error:null})
+    try {
+        const response = await axiosInstance.get("/songs/trending")
+        set({trendingSongs:response.data})        
+    } catch (error:any) {
+        set({error:error.response.data.message})
+    }finally{
+        set({isLoading:false})
+    }
+ } 
 }))
