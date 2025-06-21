d5bd29bc4c105e7005ee16b2c70dae3096c8c255
Abhishek <abhishekparmar162@gmail.com>
2025-06-21 17:52:09 +0530

    added cron job

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,37 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/Home/HomePage"
-import AuthCallback from "./pages/Auth-callback/Auth-callback"
-import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
-import MainLayout from "./layout/MainLayout"
-import ChatPage from "./pages/chat/ChatPage"
-import AlbumPage from "./pages/album/AlbumPage"
-import AdminPage from "./pages/admin/AdminPage"
-import {Toaster} from 'react-hot-toast'
-import NotFoundPage from "./pages/404/NotFoundPage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import AuthCallback from "./pages/Auth-callback/Auth-callback";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 const App = () => {
   return (
     <>
- <Routes > 
-
-  <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpFallbackRedirectUrl={'/authCallback'}/>}/>
-  <Route path="/authCallback" element={<AuthCallback />} />
-  <Route path="/admin" element={<AdminPage />} />
-  <Route element={<MainLayout />}>
-  <Route path="/" element={<HomePage />}/>
-  <Route path="/chat" element={<ChatPage />} />
-  <Route path="/albums/:albumId" element={<AlbumPage />} />
-  <Route path='*' element={<NotFoundPage />} />
-  </Route>
- </Routes>
- <Toaster />
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpFallbackRedirectUrl={"/authCallback"}
+            />
+          }
+        />
+        <Route path="/authCallback" element={<AuthCallback />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <Toaster />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
