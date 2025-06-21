da347a8fd84e42f9e4f508385bbc683d58238f91
Abhishek <abhishekparmar162@gmail.com>
2025-06-21 18:12:23 +0530

    added cron job

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,11 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly MODE: string;
-  readonly VITE_DEV_API: string;
-  readonly VITE_PROD_API: string;
   readonly VITE_CLERK_PUBLISHABLE_KEY: string;
-  // add more VITE_... keys as needed
+  readonly VITE_API_URL: string;
+  readonly MODE: string;
 }
 
 interface ImportMeta {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
